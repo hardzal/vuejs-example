a1b01f1e567b5912c5ec3783e3345e46a1661c30
hardzal <suryadijogja@gmail.com>
2021-03-12 21:04:52 +0700

    version 1

--- vue-begin/example-app/7-task-tracker-app/src/components/Header.vue
+++ vue-begin/example-app/7-task-tracker-app/src/components/Header.vue
@@ -1,7 +1,11 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button text="Add Task" color="green" />
+    <Button
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+      @btn-click="$emit('toggle-add-task')"
+    />
   </header>
 </template>
 <script>
@@ -14,6 +18,7 @@ export default {
     //   type: String,
     //   default: 'Hello World',
     // },
+    showAddTask: Boolean,
     title: String,
   },
   components: {
