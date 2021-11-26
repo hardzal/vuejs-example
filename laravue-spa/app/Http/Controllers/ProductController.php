a55b43932ad17e0c80fb1f19343cb4af839681fc
hardzal <suryadijogja@gmail.com>
2021-11-26 17:53:06 +0700

    add laravel-vue

---
+++ laravue-spa/app/Http/Controllers/ProductController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all()->toArray();
+        return array_reverse($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new Product([
+            'name' => $request->input('name'),
+            'detail' => $request->input('detail')
+        ]);
+
+        $product->save();
+        return response()->json('Product created');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->name = $request->name;
+        $product->detail = $request->detail;
+
+        $product->save();
+        return response()->json('Product updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+
+        return response()->json('Product deleted!');
+    }
+}
---
+++ laravue-spa/source.md
@@ -0,0 +1,4 @@
+- https://www.positronx.io/create-laravel-vue-js-crud-single-page-application/
+- https://medium.com/swlh/laravel-8-single-page-application-spa-with-vue-js-989a5d9d7c2
+- https://mobiosolutions.com/the-ultimate-guide-to-implement-vue-js-in-laravel/
+
